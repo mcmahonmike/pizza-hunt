60fd4fadab9ddc718597764b4d57acf82ae36117
Hephaestus <mcmahonmike9@yahoo.com>
2022-07-03 14:36:10 -0500

    reply functions built

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,36 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+const ReplySchema = new Schema(
+  {
+   // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    }
+  }
+  );
+  // get total count of comments and replies on retrieval
+  CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+  });
 
 const CommentSchema = new Schema({
   writtenBy: {
@@ -10,8 +42,17 @@ const CommentSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
-});
+  },
+  replies: [ReplySchema]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+}
+);
 
 const Comment = model('Comment', CommentSchema);
 
